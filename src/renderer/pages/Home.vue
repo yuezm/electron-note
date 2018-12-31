278a7d944e0108972dfc4ca1d277567946013565
zhiming.yue <zhiming.yue@nx-engine.com>
2018-12-31 21:45:26 +0800

    feat: 初始化项目

---
+++ src/renderer/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Slider></Slider>
+  </div>
+</template>
+<script>
+import Slider from './Slider.vue';
+export default {
+  name: 'Home',
+  components: {
+    Slider,
+  },
+};
+</script>
+<style scoped lang="less">
+</style>
---
+++ src/renderer/pages/Slider.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Menu theme="light">
+      <Submenu name="MINE">
+        <template slot="title">
+          <Icon type="ios-paper" />
+          我的笔记
+        </template>
+        <menu-item :name="item.name" v-for="(item, index) in mineNoteList" :key="index">{{item.name}}</menu-item>
+      </Submenu>
+
+      <menu-item name="TRASH">
+        <Icon type="ios-paper" />
+        回收站
+      </menu-item>
+    </Menu>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Slider',
+  data() {
+    return {
+      mineNoteList: [
+        {
+          name: '第一步',
+          url: '',
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped lang="less">
+</style>
